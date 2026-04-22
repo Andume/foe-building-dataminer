4f71208891f116ab59e73b497a07168c44cfba6c
Andumé <165851999+Andume@users.noreply.github.com>
2026-04-22 15:44:18 -0700

    Update version number

--- script/init.py
+++ script/init.py
@@ -2,7 +2,7 @@ from natsort import natsorted
 import requests
 import json
 import os
-version = 'v26.04.21.0-beta' #Version number of the dataminer, used to check for updates. Format: v[YY].[MM].[DD]-[beta/main]
+version = 'v26.04.22.0-beta' #Version number of the dataminer, used to check for updates. Format: v[YY].[MM].[DD]-[beta/main]
 dir = os.path.dirname(__file__) #Get directory of the script to access local files
 def checkForUpdates():
     print('Checking for updates...')
